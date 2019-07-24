5502ca0c0a3bf9a729d5f244ccd2987af0039f7a
Tomasz Gemza <t.gemza@secit.pl>
2019-07-24 11:48:27 +0200

    Fixed validator service registration + added missing translations

---
+++ DependencyInjection/ValidationExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SecIT\ValidationBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * Class ValidationExtension.
+ *
+ * @author Tomasz Gemza
+ */
+class ValidationExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
